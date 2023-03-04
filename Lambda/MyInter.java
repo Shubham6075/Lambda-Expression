e1b282c5a73b03f6088ac474ed05ce25beb7c776
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-04 08:49:22 +0530

    Add files via upload

---
+++ Lambda/MyInter.java
@@ -0,0 +1,14 @@
+package Lambda;
+@FunctionalInterface
+public interface MyInter {
+
+	public abstract void sayHello();
+
+}
+// 3 ways of using interfaces :
+
+// 1. create separate class and implements this interface
+
+// 2. anonymous class for implementing interface
+
+// 3. Lambda Expression
