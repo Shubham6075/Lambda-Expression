bec449ac354e8d40d81f51033446c180aae99ee9
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-05 21:25:15 +0530

    Add files via upload

---
+++ Design pattern/singelton/Design pattern/factory/plan.java
@@ -0,0 +1,11 @@
+package DesignPatterns.Factory;
+
+import java.io.*;
+abstract class Plan{
+	protected double rate;
+	abstract void getRate();
+
+	public void calculateBill(int units){
+		System.out.println(units*rate);
+	}
+}
