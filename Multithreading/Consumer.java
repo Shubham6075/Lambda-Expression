ad033277b43d7696222ed030d5f6c6fd581370ea
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-16 16:07:28 +0530

    Add files via upload

---
+++ Multithreading/Consumer.java
@@ -0,0 +1,22 @@
+package Multithreading;
+
+class Consumer extends Thread{
+	Company c;
+	Consumer(Company c)
+	{
+		this.c=c;
+	}
+
+	public void run()
+	{
+		while(true)
+		{
+			try {
+				this.c.consume_item();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			try{Thread.sleep(2000);}catch(Exception e){}
+		}
+	}
+}
