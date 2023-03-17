e771eb386c0b73e88a45752d4fca884c5acb08c6
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-17 22:32:02 +0530

    Add files via upload

---
+++ Multithreading/Producer.java
@@ -0,0 +1,24 @@
+package Multithreading;
+
+class Producer extends Thread
+{
+	Company c;
+	Producer(Company c)
+	{
+		this.c=c;
+	}
+	public void run()
+	{
+		int i=1;
+		while(true)
+		{
+			try {
+				this.c.produce_item(i);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			try{Thread.sleep(1000);}catch(Exception e){}
+			i++;
+		}
+	}
+}
