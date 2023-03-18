671a2e35174c1a2e15dc2555d938caec27b000b1
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-03-18 13:33:35 +0530

    Add files via upload

---
+++ Multithreading/Main.java
@@ -0,0 +1,13 @@
+package Multithreading;
+
+class Main
+{
+	public static void main(String[] args) {
+		Company comp=new Company();
+		Producer p=new Producer(comp);
+		Consumer c=new Consumer(comp);
+		p.start();
+		c.start();
+
+	}
+}
