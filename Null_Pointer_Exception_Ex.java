2e70378d4540eba013c42547827b3eac46d9d8fa
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-06-19 08:05:33 +0530

    Add files via upload

---
+++ Null_Pointer_Exception_Ex.java
@@ -0,0 +1,14 @@
+package ExceptionHandling;
+
+public class Null_Pointer_Exception_Ex {
+	public static void main(String args[])
+	{
+		try{
+			String str=null;
+			System.out.println (str.length());
+		}
+		catch(NullPointerException e){
+			System.out.println("NullPointerException..");
+		}
+	}
+}
